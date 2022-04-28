5e3f7124272532f31aedf80d1619b0ff23b0b771
Armogo <57241958+Armogo@users.noreply.github.com>
2022-04-29 01:29:33 +0800

    feat: add data-per-page switch

---
+++ src/components/SwitchDataPerPage.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+select(class="border-2 border-neutral-400" :class="[darkMode ? 'bg-slate-800' : 'bg-slate-100']" :value="dataPerPage" @change="switchDataPerPage")
+  option(value="10" class="bg-inherit") 10
+  option(value="30" class="bg-inherit" selected) 30
+  option(value="50" class="bg-inherit") 50  
+</template>
+
+<script setup>
+defineProps({ darkMode: Boolean, dataPerPage: Number });
+const emit = defineEmits(["switchDataPerPage"]);
+
+function switchDataPerPage(event) {
+  const number = Number(event.target.value);
+
+  emit("switchDataPerPage", number);
+}
+</script>
