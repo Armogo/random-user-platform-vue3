b642794d872a5625ccf2f91f7fb9f6b0aa55e761
Armogo <57241958+Armogo@users.noreply.github.com>
2022-04-29 18:55:45 +0800

    chore: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  // extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  extends: ["plugin:vue/vue3-recommended", "prettier"],
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
