9782fcc1e9aa6c93f44f8c1ad3e7f15de225bd5a
Armogo <57241958+Armogo@users.noreply.github.com>
2022-05-01 00:58:43 +0800

    style: keep image ratio in list mode in narrow screen

--- src/components/ViewModeList.vue
+++ src/components/ViewModeList.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 main(class="overflow-y-scroll scrollbar" :class="darkMode ? 'scrollbar-dark' : 'scrollbar'")
   div(class="w-[90%] max-w-[750px] mx-auto mb-2 flex border border-solid border-slate-300 rounded shadow-lg break-words cursor-pointer" v-for="(user, index) in props.data" :key="index" @click="showModal(user)")
-    img(class="border border-slate-300 border-solid" :src="user.picture.large" :class="{'brightness-90':darkMode}" alt="user's avatar")
+    img(class="my-auto border border-slate-300 border-solid" :src="user.picture.large" :class="{'brightness-90':darkMode}" alt="user's avatar")
     div(class="mx-auto px-3 pt-1 pb-2 text-center")
       p(class="font-bold") {{user.name.last}} {{user.name.first }}
       div(class="text-sm")
